8e9ca47ce3a170edfcdfc364ba112968c66062a1
Zhao Wang <wangz2266@gmail.com>
2019-12-19 01:08:51 +0000

    while and for loop packing

--- packing-list.js
+++ packing-list.js
@@ -11,7 +11,14 @@ const packingList = [
 
 //packing
 
+// console.log("Kitchen stuff to pack:");
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
